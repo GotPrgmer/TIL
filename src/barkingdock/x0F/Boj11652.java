6564f879ad17d6ba90b337147dc7ac0d03f65bfd
Gotprgmer <gofiction95@gmail.com>
2025-10-16 20:33:14 +0900

    Boj11652 최적화

--- src/barkingdock/x0F/Boj11652.java
+++ src/barkingdock/x0F/Boj11652.java
@@ -6,16 +6,17 @@ public class Boj11652 {
     public static void main(String[] argS) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        arr = new long[n];
-        for(int i=0;i<arr.length;i++){
+        arr = new long[n+1];
+        for(int i=0;i<n;i++){
             arr[i] = Long.parseLong(br.readLine());
         }
-        int cnt = 0;
+        int cnt = 1;
         long maxVal = -Long.MAX_VALUE;
         int maxCnt = 0;
+        arr[n] = Long.MAX_VALUE;
         Arrays.sort(arr);
-        for(int i=0;i<arr.length;i++){
-            if(i==0 || arr[i-1] == arr[i]) cnt++;
+        for(int i=1;i<arr.length;i++){
+            if( arr[i-1] == arr[i]) cnt++;
             else{
                 if(cnt > maxCnt){
                     maxCnt = cnt;
@@ -23,9 +24,6 @@ public class Boj11652 {
                 }
                 cnt = 1;
             }
-        }
-        if(cnt > maxCnt){
-            maxVal = arr[n-1];
         }
         System.out.println(maxVal);
     }
