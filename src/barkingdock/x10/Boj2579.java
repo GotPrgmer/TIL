8edf59dfa2b053fd9108d65ef12282f10991db6d
Gotprgmer <gofiction95@gmail.com>
2025-10-17 12:47:32 +0900

    Boj2579 2차 배열로 최적화

--- src/barkingdock/x10/Boj2579.java
+++ src/barkingdock/x10/Boj2579.java
@@ -9,15 +9,19 @@ public class Boj2579 {
         for(int i=1;i<n+1;i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
-        int[] dp = new int[n+1];
-        dp[1] = arr[1];
-
+        int[][] dp = new int[n+1][3];
+        dp[1][1] = arr[1];
+        dp[1][2] = arr[1];
         if(n>=2){
-            dp[2] = dp[1]+arr[2];
+            dp[2][1] = arr[2];
+            dp[2][2] = dp[1][1]+arr[2];
             for(int i=3;i<n+1;i++){
-                dp[i] = Math.max(dp[i-2],dp[i-3]+arr[i-1])+arr[i];
+                dp[i][1] = Math.max(dp[i-2][1],dp[i-2][2])+arr[i];
+                dp[i][2] = dp[i-1][1] + arr[i];
             }
         }
-        System.out.println(dp[n]);
+        System.out.println(Math.max(dp[n][1], dp[n][2]));
+
+
     }
 }
