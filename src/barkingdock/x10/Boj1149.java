f64cb1038cef7611cf23fe293618075bdec771d2
Gotprgmer <gofiction95@gmail.com>
2025-10-18 17:48:29 +0900

    Boj1149 최적화

--- src/barkingdock/x10/Boj1149.java
+++ src/barkingdock/x10/Boj1149.java
@@ -43,11 +43,7 @@ public class Boj1149 {
         dp[1][1] = arr[1][1];
         dp[1][2] = arr[1][2];
 
-        dp[2][0] = Math.min(dp[1][1], dp[1][2])+arr[2][0];
-        dp[2][1] = Math.min(dp[1][0], dp[1][2])+arr[2][1];
-        dp[2][2] = Math.min(dp[1][0], dp[1][1])+arr[2][2];
-
-        for(int i=3;i<n+1;i++){
+        for(int i=2;i<n+1;i++){
             dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2])+arr[i][0];
             dp[i][1] = Math.min(dp[i-1][2], dp[i-1][0])+arr[i][1];
             dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1])+arr[i][2];
