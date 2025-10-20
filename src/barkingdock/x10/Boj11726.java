06bb4a318627631b531c32ddfce3971c64079c84
Gotprgmer <gofiction95@gmail.com>
2025-10-20 20:59:15 +0900

    Boj11726

--- src/barkingdock/x10/Boj11726.java
+++ src/barkingdock/x10/Boj11726.java
@@ -5,16 +5,15 @@ public class Boj11726 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
+        long[] dp = new long[n+1];
         dp[1] = 1;
         if(n>=2){
             dp[2] = 2;
             for(int i=3;i<n+1;i++){
-                dp[i] = (dp[i-1]+dp[i-2])%10007;
+                dp[i] = (dp[i-1]+dp[i-2]);
             }
         }
-        System.out.println(dp[n]);
-
+        System.out.println(dp[n]%10007);
 
     }
 
