26ff45033488266b5028a1c566f12392e3c6edb7
Gotprgmer <gofiction95@gmail.com>
2025-10-28 22:37:54 +0900

    Problem2

--- leetcode/src/Problem2.java
+++ leetcode/src/Problem2.java
@@ -22,3 +22,40 @@ public class Problem2 {
         return output.next;
     }
 }
+
+class ListNode {
+    int val;          // 노드가 저장하는 값
+    ListNode next;    // 다음 노드 참조
+
+    // 기본 생성자
+    ListNode() {}
+
+    // 값만 받는 생성자
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    // 값과 다음 노드 모두 받는 생성자
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    // getter
+    public int getVal() {
+        return val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    // setter
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
