390a00b9ae840df2863e715e5e67b15c84e8f826
Gotprgmer <gofiction95@gmail.com>
2025-10-29 12:13:44 +0900

    boj1941 not finished

--- boj/src/problems/_1941.java
+++ boj/src/problems/_1941.java
@@ -1,13 +1,14 @@
-//package problems;
-//
-//import java.io.IOException;
-//
-//public class _1941 {
-//    static int[] d_r = {-1,0,1,0};
-//    static int[] d_c = {0,1,0,-1}
-//
-//    public static void main(String[] args) throws IOException {
-//
-//    }
-//    public void backtrack(int r, int c, )
-//}
+package boj.src.problems;
+
+import java.io.IOException;
+/*Todo
+boj 1941 풀이하기
+* */
+public class _1941 {
+    static int[] d_r = {-1,0,1,0};
+    static int[] d_c = {0,1,0,-1};
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
