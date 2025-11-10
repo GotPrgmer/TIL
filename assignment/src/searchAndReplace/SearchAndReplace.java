d871aebe7700695fe0c9a8c8f83233101767fe94
Gotprgmer <gofiction95@gmail.com>
2025-11-10 15:40:53 +0900

    SearchAndReplace 찾는 모든 fins word 바꾸기

--- assignment/src/searchAndReplace/SearchAndReplace.java
+++ assignment/src/searchAndReplace/SearchAndReplace.java
@@ -2,14 +2,15 @@ package assignment.src.searchAndReplace;
 
 public class SearchAndReplace {
     public static void main(String[] args){
-        System.out.println(searchAndReplace("i ate apples today", "apple", "banana"));
+        System.out.println(searchAndReplace("i ate apples apple today", "apple", "banana"));
     }
     static String searchAndReplace(String sentence, String find, String replace){
         StringBuilder sb = new StringBuilder();
         sb.append(sentence);
         int idx = sb.indexOf(find);
-        if(idx != -1){
+        while(idx!=-1){
             sb.replace(idx,idx+find.length(),replace);
+            idx = sb.indexOf(find);
         }
         return sb.toString();
     }
