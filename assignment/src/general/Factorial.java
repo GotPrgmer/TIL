c14c97271bc41f0f42f0552c29ba721f2605af42
Gotprgmer <gofiction95@gmail.com>
2025-11-10 18:38:08 +0900

    팩토리얼 문제 최적화

--- assignment/src/general/Factorial.java
+++ assignment/src/general/Factorial.java
@@ -1,40 +1,32 @@
 package assignment.src.general;
-
-import java.util.Scanner;
-
+/*
+어떤 수를 입력하더라도 해당 수가 팩토리얼 수 인지를 판단하는 프로그램을 만들자
+ */
+import java.math.BigInteger;
+import java.io.*;
 public class Factorial {
-    public static void main (String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int n;
-
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("N을 입력하세요.");
-        n = scanner.nextInt();
+        String n = br.readLine();
 
         System.out.println(solve(n));
 
     }
 
-    public static int solve(int n){
-        int value = 2;
-        int ans = 2;
-        boolean flag = false;
-        while (value<=n){
-            if(value==n){
-                flag = true;
-                break;
-            }
-            else{
-                value *= (value+1);
-                ans += 1;
-            }
-
+    public static boolean solve(String n){
+        BigInteger target;
+        target = new BigInteger(n);
+        if (target.equals(BigInteger.ONE)) {
+            return true;
         }
-        if(flag){
-            return ans;
-        }
-        else{
-            return -1;
+        BigInteger current = BigInteger.ONE;
+        BigInteger num = BigInteger.ONE;
+        while (current.compareTo(target) < 0) {
+            current = current.multiply(num);
+            num = num.add(BigInteger.ONE);
         }
 
+        return current.compareTo(target) == 0;
     }
 }
