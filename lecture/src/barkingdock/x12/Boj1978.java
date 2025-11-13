f8f270d04546fcfbd7a8cefd84ac3dce6125e303
Gotprgmer <gofiction95@gmail.com>
2025-11-13 14:10:12 +0900

    Boj1978

---
+++ lecture/src/barkingdock/x12/Boj1978.java
@@ -0,0 +1,26 @@
+package lecture.src.barkingdock.x12;
+import java.util.*;
+import java.io.*;
+public class Boj1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int ans = 0;
+        for (int t = 0; t < N; t++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(num == 1) continue;
+            boolean isPrime = true;
+            for(int i=2;i*i<=num;i++){
+                if(num%i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
