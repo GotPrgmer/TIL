c2ddbe6df3da1ba46630cb78d07aba9b0f728ef8
Gotprgmer <gofiction95@gmail.com>
2025-11-16 23:25:04 +0900

    Boj1629 최적화

--- lecture/src/barkingdock/x0B/Boj1629.java
+++ lecture/src/barkingdock/x0B/Boj1629.java
@@ -9,28 +9,39 @@ public class Boj1629 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        System.out.println(cal(A,B,C));
+         int A = Integer.parseInt(st.nextToken());
+         int B = Integer.parseInt(st.nextToken());
+         int C = Integer.parseInt(st.nextToken());
+        System.out.println(solve(A,B,C));
 
     }
-    static long cal(int A, int B, int C) {
-        if(B==0){
-            return 1;
-        }
-        else if(B==1){
-            return A%C;
-        }
-        else{
-            long val = cal(A,B/2,C)%C;
-            val = val*val%C;
-            if(B%2 == 0){
-                return val;
-            }
-            else{
-                return val*A%C;
-            }
-        }
+
+    //현재 B만큼 곱함 (1부터 시작)
+    //1 -> 2,3 식으로 나감
+    static long solve(int A, int B, int C){
+        if(B==1) return A%C;
+        long val = solve(A, B / 2, C);
+        val = val * val % C;
+        if(B%2==0) return val;
+        return val*A%C;
     }
 }
+
+//    static long cal(int A, int B, int C) {
+//        if(B==0){
+//            return 1;
+//        }
+//        else if(B==1){
+//            return A%C;
+//        }
+//        else{
+//            long val = cal(A,B/2,C)%C;
+//            val = val*val%C;
+//            if(B%2 == 0){
+//                return val;
+//            }
+//            else{
+//                return val*A%C;
+//            }
+//        }
+//    }
